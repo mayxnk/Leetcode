5bb54207f5f9297fd9a94ff1b826ff4d72beb87f
mayxnk <105148831+mayxnk@users.noreply.github.com>
2022-06-01 01:00:06 +0530

    Time: 646 ms (32.07%), Space: 71.8 MB (37.11%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,52 +1,63 @@
 class Solution {
 public:
-    void helper(vector<string>& bCode,int k,int i,string str)
+   void helper(vector<string>& bCode,int k,int i,int *a)
     {
         //base case
         if(i == k)
         {
+        	string str = "",temp = "";
+			// for(int i = 0;i<k;i++)
+			// {
+			// 	temp.append(to_string(a[i]));
+			// }
+            transform(a,a+k,back_inserter(str),[](int const &i) {
+                        return i + '0';
+                    });
             bCode.push_back(str);
             return;
         }
-        str[i] = 0;
-        helper(bCode,k,i+1,str);
-        str[i] = 1;
-        helper(bCode,k,i+1,str);
+        a[i] = 0;
+        helper(bCode,k,i+1,a);
+        a[i] = 1;
+        helper(bCode,k,i+1,a);
     }
     bool hasAllCodes(string s, int k) 
     {
-//         if(s.size()==0 && k!=0)
-//             return false;
-//         vector<string> bCode;
-//         string str;
-//         helper(bCode,k,0,str);
+        //Brute force
+        // if((s.size()==0 && k!=0) || k>s.size())
+        //     return false;
+        // if(s.size() == 1)
+        // {
+        //     if(k >= 1)
+        //         return false;
+        //     else
+        //         return true;
+        // }
+        // vector<string> bCode;
+        // int str[k];
+        // helper(bCode,k,0,str);
+        // int count = 0;
+        // // for(int i = 0;i<bCode.size();i++)
+        // // cout<<bCode[i]<<endl;
+        // for(int i = 0;i<=s.size()-k;i++)
+        // {
+        //     string temp = s.substr(i,k);
+        //     for(int j = 0;j<bCode.size();j++)
+        //     {
+        //         if(temp == bCode[j])
+        //             count++;
+        //     }
+        // }
+        // if(count<bCode.size())
+        //     return false;
+        // return true;
         
-//         for(int i = 0;i<bCode.size();i++)
-//         {
-//             string temp = bCode[i];
-//             int j,count = 0;
-//             for(j = 0;j<=s.size()-k;j++)
-//             {
-//                 string s1 = s.substr(j,k);
-//                 if(temp == s1)
-//                 {
-//                     count++;
-//                     break;
-//                 }
-//             }
-//             if(count == 0)
-//                 return false;
-//         }
-//         return true;
         if(k > s.size()) return false;
-    
-    unordered_set<string> res_set;
-    
-	//Insertion into the set
-    for(int i =0;i<= s.size()-k;i++)
-        res_set.insert(s.substr(i,k));
-    
-	//Comparing res with 2^k
-    return res_set.size() == pow(2,k);
-    }
+        unordered_set<string> bCode;
+        
+        for(int i = 0;i<=s.size()-k;i++)
+            bCode.insert(s.substr(i,k));
+        
+        return bCode.size() == pow(2,k);
+}
 };
