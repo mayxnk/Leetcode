76d1bb435bf50513f55f0cd6886927c3d4e0716b
mayxnk <105148831+mayxnk@users.noreply.github.com>
2022-06-11 01:10:02 +0530

    Time: 3 ms (98.61%), Space: 6.6 MB (99.89%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -5,6 +5,7 @@ public:
         if(s.size()==0 || s.size()==1)
             return s.size();
         //Brute Force Approach
+        /*
         unordered_set<char> unique;
         int len,maxm = 0;
         for(int i = 0;i<s.size();i++)
@@ -19,6 +20,23 @@ public:
             }
             maxm = max(maxm,len);
             unique.clear();
+        }
+        return maxm;
+        */
+        //Optimal Approach - Time:O(2*n),Space:O(m)
+        /**Sliding window technique**/
+        int count[256] = {0};
+        int maxm = 0,left = 0,right = 0;
+        while(right<s.size())
+        {
+            count[s[right]]++;//Adding occurences of character
+            while(count[s[right]] > 1)
+            {
+                count[s[left]]--;
+                left++;
+            }
+            maxm = max(maxm,right-left+1);
+            ++right;
         }
         return maxm;
     }
