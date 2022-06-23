a2b6be1e58f3b07747394036aabf6ba9a627d0c8
mayxnk <105148831+mayxnk@users.noreply.github.com>
2022-06-24 04:04:47 +0530

    Time: 421 ms (34.09%), Space: 56.1 MB (85.88%) - LeetHub

---
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    static bool cmp(vector<int>& a, vector<int>& b)
+    {
+        return a[1] < b[1];
+    }
+    int scheduleCourse(vector<vector<int>>& courses) 
+    {
+        //sorting on basis of last day
+        sort(courses.begin(),courses.end(),cmp);
+        int time = 0;
+        priority_queue<int> maxm;
+        
+        for(auto &course : courses)
+        {
+            //If total prev. time + current time is less than equal to current 
+            if(time + course[0] <= course[1])
+            {
+                time += course[0];
+                maxm.push(course[0]);
+            }
+            //Replacing previous longest course with current course
+            else if(!maxm.empty() && maxm.top() >= course[0])
+            {
+                time -= maxm.top();
+                time += course[0];
+                maxm.pop();
+                maxm.push(course[0]);
+            }
+        }
+        return maxm.size();
+    }
+};
