ea22f52bd9fcac051866ebc932f74c4398462509
mayxnk <105148831+mayxnk@users.noreply.github.com>
2022-06-26 01:15:46 +0530

    Time: 19 ms (13.66%), Space: 9.8 MB (93.36%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -2,13 +2,32 @@ class Solution {
 public:
     bool checkIfExist(vector<int>& arr) 
     {
-        unordered_map<int,int> mp;
-        for(int i = 0;i<arr.size();++i)
-            mp[arr[i]] = i;
+        /** Time - O(n), Space - O(n) **/
+        // unordered_map<int,int> mp;
+        // for(int i = 0;i<arr.size();++i)
+        //     mp[arr[i]] = i;
+        // for(int i = 0;i<arr.size();++i)
+        // {
+        //     if(mp.find(arr[i]*2) != mp.end() and i != mp[arr[i]*2])
+        //         return true;
+        // }
+        // return false;
+        
+        /** **/
+        sort(arr.begin(),arr.end());
         for(int i = 0;i<arr.size();++i)
         {
-            if(mp.find(arr[i]*2) != mp.end() and i != mp[arr[i]*2])
-                return true;
+            int key = arr[i] * 2,s = 0,e = arr.size()-1;
+            while(s <= e)
+            {
+                int mid = (s+e)/2;
+                if(arr[mid] == key and mid != i)
+                    return true;
+                else if(arr[mid] < key)
+                    s = mid + 1;
+                else
+                    e = mid -1;
+            }
         }
         return false;
     }
