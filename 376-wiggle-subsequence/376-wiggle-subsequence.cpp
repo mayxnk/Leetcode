d2e428e8de348924d63b47b3c5bcf7888ee154ad
mayxnk <105148831+mayxnk@users.noreply.github.com>
2022-07-04 01:31:02 +0530

    Time: 7 ms (39.69%), Space: 7.2 MB (43.96%) - LeetHub

---
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    //1 -> positive
+    //0 -> negative
+    int dp[1001][2];
+    int helper(vector<int>& nums,int sign,int i)
+    {
+        if(i == nums.size())
+            return 0;
+        if(dp[i][sign] != -1)
+            return dp[i][sign];
+        int ans1 = INT_MIN;
+        if(sign)
+        {
+            if(nums[i] > nums[i-1])
+                ans1 = max(helper(nums,!sign,i+1) + 1,ans1);
+            ans1 = max(helper(nums,sign,i+1),ans1);
+        }
+        else
+        {
+            if(nums[i] < nums[i-1])
+                ans1 = max(helper(nums,!sign,i+1) + 1,ans1);
+            ans1 = max(helper(nums,sign,i+1),ans1);
+        }
+        
+        return dp[i][sign] = ans1;
+    }
+    int wiggleMaxLength(vector<int>& nums) 
+    {
+        memset(dp,-1,sizeof(dp));
+        return 1 + max(helper(nums,1,1),helper(nums,0,1));
+    }
+};
