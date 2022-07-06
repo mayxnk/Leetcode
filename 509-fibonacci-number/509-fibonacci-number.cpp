ac43ccf15156dcddd186a1af727e30c49ac23f1e
mayxnk <105148831+mayxnk@users.noreply.github.com>
2022-07-07 03:05:10 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (9.60%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    int helper(int n,vector<int>& a)
-    {
-        if(n<=1)
-            return n;
-        if(a[n]!=-1)
-            return a[n];
-        return a[n] = (helper(n-1,a) + helper(n-2,a));
-    }
+    // int helper(int n,vector<int>& a)
+    // {
+    //     if(n<=1)
+    //         return n;
+    //     if(a[n]!=-1)
+    //         return a[n];
+    //     return a[n] = (helper(n-1,a) + helper(n-2,a));
+    // }
     int fib(int n) {
         //recusive
         // if(n <=1 )
@@ -15,8 +15,13 @@ public:
         // return fib(n-1) + fib(n-2);
         if(n<=1)
             return n;
-        vector<int> a(n+1,-1);
-        helper(n,a);
+        vector<int> a(n+1);
+        //helper(n,a);
+        a[0] = 0,a[1] = 1;
+        for(int i = 2;i<=n;++i)
+        {
+            a[i] = a[i-1] + a[i-2];
+        }
         return a[n];
     }
 };
