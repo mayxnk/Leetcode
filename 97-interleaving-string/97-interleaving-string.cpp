71a91fb839d686850957c8159fdbba47c70ca31c
mayxnk <105148831+mayxnk@users.noreply.github.com>
2022-07-09 02:04:16 +0530

    Time: 53 ms (20.55%), Space: 25.1 MB (24.50%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,40 +1,34 @@
 class Solution {
 public:
+    int dp[102][102];
+    bool solve(string s1,string s2,string s3,int i,int j,int k)
+    {
+        if(i == s1.size() and j == s2.size() and k == s3.size())
+        {
+            return true;
+        }
+        if(i>s1.size() || j>s2.size())
+            return false;
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        
+        if(s1[i] == s3[k] and s2[j] == s3[k])
+        {
+            return dp[i][j] = solve(s1,s2,s3,i+1,j,k+1) || solve(s1,s2,s3,i,j+1,k+1);
+        }
+        else if(s1[i] == s3[k])
+            return dp[i][j] = solve(s1,s2,s3,i+1,j,k+1);
+        else if(s2[j] == s3[k])
+            return dp[i][j] = solve(s1,s2,s3,i,j+1,k+1);
+        else
+            return false;
+            
+    }
     bool isInterleave(string s1, string s2, string s3) 
     {
-        int n=s1.length();
-        int m=s2.length();
-        if(n+m!=s3.length())
-            return 0;
-        bool dp[n+1][m+1];
-        memset(dp,0,sizeof(dp));
-        
-        for(int i=0;i<=n;i++)
-        {
-            for(int j=0;j<=m;j++)
-            {
-                if(i==0&&j==0)
-                    dp[i][j]=true;
-                else if(i==0)
-                {
-                    if(s2[j-1]==s3[j-1])
-                        dp[i][j]=dp[i][j-1];
-                }
-                else if(j==0)
-                {
-                    if(s1[i-1]==s3[i-1])
-                        dp[i][j]=dp[i-1][j];
-                }
-                else if(s1[i-1]==s3[i+j-1]&&s2[j-1]!=s3[i+j-1])
-                    dp[i][j]=dp[i-1][j];
-                
-                else if(s1[i-1]!=s3[i+j-1]&&s2[j-1]==s3[i+j-1])
-                    dp[i][j]=dp[i][j-1];
-                
-                else if(s1[i-1]==s3[i+j-1]&&s2[j-1]==s3[i+j-1])
-                    dp[i][j]=(dp[i-1][j] || dp[i][j-1]);
-            }
-        }
-        return dp[n][m];
+        if(s3.size() != s1.size() + s2.size())
+			return false;
+        memset(dp,-1,sizeof(dp));
+        return solve(s1,s2,s3,0,0,0);
     }
 };
