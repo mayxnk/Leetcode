c23a3af754d0f499cdedab61aeb783efc0c87f57
mayxnk <105148831+mayxnk@users.noreply.github.com>
2022-07-11 22:56:35 +0530

    Time: 11 ms (15.81%), Space: 11.9 MB (66.66%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -11,12 +11,22 @@
  */
 class Solution {
 public:
+    void helper(TreeNode* root,vector<int>& ans,int level)
+    {
+        if(root == NULL)
+            return;
+        if(ans.size()<=level)
+            ans.push_back(root->val);
+        helper(root->right,ans,level+1);
+        helper(root->left,ans,level+1);
+    }
     vector<int> rightSideView(TreeNode* root) 
     {
         if(root == NULL)
             return {};
         vector<int> ans;
         //BFS Approach
+       /*
         queue<TreeNode*> q;
         q.push(root);
         while(q.size()!=0)
@@ -35,5 +45,9 @@ public:
             ans.push_back(currNode->val);
         }
         return ans;
+        */
+        helper(root,ans,0);
+        return ans;
+        
     }
 };
