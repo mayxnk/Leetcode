f536500dc800e10fe877aca036e95cdc59e01372
mayxnk <105148831+mayxnk@users.noreply.github.com>
2022-07-25 18:17:52 +0530

    Time: 147 ms (65.66%), Space: 14.9 MB (21.86%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -2,15 +2,17 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) 
     {
+        //Binary Search Approach : T - O(n+m)
         if(matrix.size() == 0)
             return false;
         int row = matrix.size(),col = matrix[0].size();
         int i = 0,j = col - 1;
         while(i < row and j >=0)
         {
-            if(matrix[i][j] == target)
+            int element = matrix[i][j];
+            if(element == target)
                 return true;
-            else if(matrix[i][j] > target)
+            else if(element > target)
                 --j;
             else
                 ++i;
