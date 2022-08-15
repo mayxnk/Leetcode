8055a3b0b535e29d088f3ee469f4ef2ff7e3cba2
mayxnk <105148831+mayxnk@users.noreply.github.com>
2022-08-15 21:49:47 +0530

    Time: 22 ms (42.26%), Space: 8.1 MB (37.92%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -8,64 +8,74 @@ public:
 //         mp["IV"] = 4,mp["IX"] = 9,mp["XL"] = 40,mp["XC"] = 90;
 //         mp["CP"] = 400,mp["CM"] = 900;
         
-        int sum = 0,i = 0,size = s.size();
+         int sum = 0,i = 0,size = s.size();
+        // while(i<size)
+        // {
+        //     if(s[i] == 'I')
+        //     {
+        //         if(i<size-1 and s[i+1] == 'V')
+        //         {
+        //             sum += 4;
+        //             i+=2;
+        //             continue;
+        //         }
+        //         else if(i<size-1 and s[i+1] == 'X')
+        //         {
+        //             sum += 9;
+        //             i+=2;
+        //             continue;
+        //         }
+        //         else
+        //             ++sum;
+        //     }
+        //     else if(s[i] == 'X')
+        //     {
+        //         if(i<size-1 and s[i+1] == 'L')
+        //         {
+        //             sum += 40;
+        //             i+=2;
+        //             continue;
+        //         }
+        //         else if(i<size-1 and s[i+1] == 'C')
+        //         {
+        //             sum += 90;
+        //             i+=2;
+        //             continue;
+        //         }
+        //         else
+        //             sum += 10;
+        //     }
+        //     else if(s[i] == 'C')
+        //     {
+        //         if(i<size-1 and s[i+1] == 'D')
+        //         {
+        //             sum += 400;
+        //             i+=2;
+        //             continue;
+        //         }
+        //         else if(i<size-1 and s[i+1] == 'M')
+        //         {
+        //             sum += 900;
+        //             i+=2;
+        //             continue;
+        //         }
+        //         else
+        //             sum += 100;
+        //     }
+        //     else
+        //         sum += mp[s[i]];
+        //     ++i;
+        // }
+        // return  sum;
+        /*Approach 2*/
         while(i<size)
         {
-            if(s[i] == 'I')
-            {
-                if(i<size-1 and s[i+1] == 'V')
-                {
-                    sum += 4;
-                    i+=2;
-                    continue;
-                }
-                else if(i<size-1 and s[i+1] == 'X')
-                {
-                    sum += 9;
-                    i+=2;
-                    continue;
-                }
-                else
-                    ++sum;
-            }
-            else if(s[i] == 'X')
-            {
-                if(i<size-1 and s[i+1] == 'L')
-                {
-                    sum += 40;
-                    i+=2;
-                    continue;
-                }
-                else if(i<size-1 and s[i+1] == 'C')
-                {
-                    sum += 90;
-                    i+=2;
-                    continue;
-                }
-                else
-                    sum += 10;
-            }
-            else if(s[i] == 'C')
-            {
-                if(i<size-1 and s[i+1] == 'D')
-                {
-                    sum += 400;
-                    i+=2;
-                    continue;
-                }
-                else if(i<size-1 and s[i+1] == 'M')
-                {
-                    sum += 900;
-                    i+=2;
-                    continue;
-                }
-                else
-                    sum += 100;
-            }
+            if(i<size-1 and mp[s[i]]<mp[s[i+1]])
+                sum -= mp[s[i]];
             else
                 sum += mp[s[i]];
             ++i;
         }
-        return  sum;
+        return sum;
     }
 };
