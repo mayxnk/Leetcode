89145972557a1ebb602edb36cf7a063ab5b46ca0
mayxnk <105148831+mayxnk@users.noreply.github.com>
2022-08-15 21:37:45 +0530

    Time: 20 ms (46.93%), Space: 8 MB (40.48%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+    int romanToInt(string s) 
+    {
+        unordered_map<char,int> mp;
+        mp['I'] = 1,mp['V'] = 5,mp['X'] = 10,mp['L'] = 50;
+        mp['C'] = 100,mp['D'] = 500,mp['M'] = 1000;
+//         mp["IV"] = 4,mp["IX"] = 9,mp["XL"] = 40,mp["XC"] = 90;
+//         mp["CP"] = 400,mp["CM"] = 900;
+        
+        int sum = 0,i = 0,size = s.size();
+        while(i<size)
+        {
+            if(s[i] == 'I')
+            {
+                if(i<size-1 and s[i+1] == 'V')
+                {
+                    sum += 4;
+                    i+=2;
+                    continue;
+                }
+                else if(i<size-1 and s[i+1] == 'X')
+                {
+                    sum += 9;
+                    i+=2;
+                    continue;
+                }
+                else
+                    ++sum;
+            }
+            else if(s[i] == 'X')
+            {
+                if(i<size-1 and s[i+1] == 'L')
+                {
+                    sum += 40;
+                    i+=2;
+                    continue;
+                }
+                else if(i<size-1 and s[i+1] == 'C')
+                {
+                    sum += 90;
+                    i+=2;
+                    continue;
+                }
+                else
+                    sum += 10;
+            }
+            else if(s[i] == 'C')
+            {
+                if(i<size-1 and s[i+1] == 'D')
+                {
+                    sum += 400;
+                    i+=2;
+                    continue;
+                }
+                else if(i<size-1 and s[i+1] == 'M')
+                {
+                    sum += 900;
+                    i+=2;
+                    continue;
+                }
+                else
+                    sum += 100;
+            }
+            else
+                sum += mp[s[i]];
+            ++i;
+        }
+        return  sum;
+    }
+};
