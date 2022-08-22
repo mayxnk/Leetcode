139afdfb370d6ac22b4588842b923f7f8e9a5942
mayxnk <105148831+mayxnk@users.noreply.github.com>
2022-08-23 01:10:02 +0530

    Time: 3 ms (58.67%), Space: 5.9 MB (24.97%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
-    bool isPowerOfFour(int n) 
-    {
-        if(n==0)
-            return false;
-        if(n==1)
-            return true;
-        return (n%4==0 and isPowerOfFour(n/4));
+    bool isPowerOfFour(int n) {
+    // {
+    //     if(n==0)
+    //         return false;
+    //     if(n==1)
+    //         return true;
+    //     return (n%4==0 and isPowerOfFour(n/4));
+        //Optimised
+        //check three conditions:
+        /*1.greater than n
+        2.only one set bit
+        3.Set bit at odd position
+        */
+        return (n>0) and !(n&(n-1)) and (n & 0x55555555);
     }
 };
