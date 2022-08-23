cf2e0617a83475a5f0872bbefdb5f60bc1108dcc
mayxnk <105148831+mayxnk@users.noreply.github.com>
2022-08-24 03:13:22 +0530

    Time: 242 ms (94.21%), Space: 110.4 MB (94.47%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -12,20 +12,41 @@ class Solution {
 public:
     bool isPalindrome(ListNode* head) 
     {
-        stack<ListNode*> s;
-        ListNode* temp = head;
-        while(temp!=NULL)
+        // stack<ListNode*> s;
+        // ListNode* temp = head;
+        // while(temp!=NULL)
+        // {
+        //     s.push(temp);
+        //     temp = temp->next;
+        // }
+        // temp = head;
+        // while(s.size())
+        // {
+        //     if(s.top()->val != temp->val)
+        //         return false;
+        //     s.pop();
+        //     temp = temp->next;
+        // }
+        // return true;
+        
+        // Without using space,Time - O(n)
+        ListNode* prev = NULL,*slow = head,*fast = head,*temp;
+        while(slow and fast and fast->next)
         {
-            s.push(temp);
-            temp = temp->next;
+            fast = fast->next->next;
+            temp = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = temp;
         }
-        temp = head;
-        while(s.size())
+        if(fast) //no. of nodes is odd
+            slow = slow->next;
+        while(slow and prev)
         {
-            if(s.top()->val != temp->val)
+            if(slow->val != prev->val)
                 return false;
-            s.pop();
-            temp = temp->next;
+            slow = slow->next;
+            prev = prev->next;
         }
         return true;
     }
