d4a47769befb222137aac7264bb13eaa0662ba40
mayxnk <105148831+mayxnk@users.noreply.github.com>
2022-08-26 01:51:54 +0530

    Time: 35 ms (34.41%), Space: 9 MB (15.41%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canConstruct(string r, string m) 
+    {
+        unordered_map<char,int> mp1,mp2;
+        for(auto x:r)
+            mp1[x]++;
+        for(auto x:m)
+            mp2[x]++;
+        
+        for(int i = 0;i<r.size();i++)
+        {
+            if(mp2.find(r[i]) == mp2.end() or mp1[r[i]] > mp2[r[i]])
+                return false;
+        }
+        return true;
+    }
+};
