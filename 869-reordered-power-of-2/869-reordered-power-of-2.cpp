8e4abb659aacfa7ee4a949971d347f00346f5405
mayxnk <105148831+mayxnk@users.noreply.github.com>
2022-08-29 15:09:22 +0530

    Time: 9 ms (20.42%), Space: 7.6 MB (5.52%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,15 +1,35 @@
 class Solution {
 public:
     bool reorderedPowerOf2(int N) {
-         string N_str = sorted_num(N);
-        for (int i = 0; i < 32; i++)
-            if (N_str == sorted_num(1 << i)) return true;
+        //Counting of digits approach
+        map<int,vector<int>> fq;
+        int x = pow(10,9);
+        for(int i = 1;i<=x;i*=2) //log(10^9)
+        {
+            vector<int> temp(10,0);
+            int k = i;
+            while(k>0) //max nine times
+            {
+                temp[k%10]++;
+                k/=10;
+            }
+            fq[i] = temp;
+        }
+        vector<int> num(10,0);
+        for(int i = N;i>0;i/=10)
+        {
+            num[i%10]++;
+        }
+        for(int i = 1;i<=x;i*=2)
+        {
+            vector<int> temp = fq[i];
+            bool flag = true;
+            for(int j = 0;j<=9;++j)
+                if(temp[j] != num[j])
+                    flag = false;
+            if(flag)
+                return true;
+        }
         return false;
-    }
-    
-    string sorted_num(int n) {
-        string res = to_string(n);
-        sort(res.begin(), res.end());
-        return res;
     }
 };
