284cdc6cb46bc6fb7b1c8bd2fa14d6238f113a46
Mayank Rastogi <105148831+mayxnk@users.noreply.github.com>
2022-12-01 01:27:41 +0530

    Time: 4 ms (69.46%), Space: 8.3 MB (20.86%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        map<int,int> mp;
+        for(auto x:arr)
+            mp[x]++;
+        set<int> st;
+        for(auto x:mp)
+        {
+            st.insert(x.second);
+        }
+        return st.size() == mp.size();
+    }
+};
